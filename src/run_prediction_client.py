2a72063db018d6f73ebfcf2e3c6f6d51547089aa
Camilo-UbuntuPC <camilo.jara@ug.uchile.cl>
2021-11-12 16:21:33 -0300

    remove sensitivity

--- src/run_prediction_client.py
+++ src/run_prediction_client.py
@@ -4,7 +4,8 @@ import asyncio
 # The main function that will handle connection and communication 
 # with the server
 async def listen():
-    url = "ws://127.0.0.1:7890"
+    #url = "ws://127.0.0.1:7890"
+    url = "ws://190.162.196.242:25565"
     # Connect to the server
     async with websockets.connect(url) as ws:
         # Send a greeting message
