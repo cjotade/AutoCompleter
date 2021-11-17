ccfbebb0a2248d23a5082fed7b3d2836fc78bdbd
Camilo-UbuntuPC <camilo.jara@ug.uchile.cl>
2021-11-16 21:10:43 -0300

    minor changes

--- src/run_prediction_client.py
+++ src/run_prediction_client.py
@@ -4,12 +4,12 @@ import asyncio
 # The main function that will handle connection and communication 
 # with the server
 async def listen():
-    #url = "ws://127.0.0.1:7890"
-    url = "ws://190.162.196.242:25565"
+    url = "ws://127.0.0.1:7890"
+    #url = "ws://190.162.196.242:25565"
     # Connect to the server
     async with websockets.connect(url) as ws:
         # Send a greeting message
-        incomplete_word = "garban"
+        incomplete_word = "ah"
         await ws.send(incomplete_word)
         # Stay alive forever, listening to incoming msgs
         while True:
